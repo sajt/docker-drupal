48a11ee7dbebdcc0f98b882a200b5082e3960c0b
Tamas Amon <tamas.amon@gmai.com>
2020-12-10 10:10:56 +0000

    Teszteléshez

--- build.sh
+++ build.sh
@@ -1,17 +1,34 @@
+myBuild() {
+    RED='\033[5;31m'
+    NC='\033[0m' # No Color
+    echo -e "${RED}Build${NC}: $buildVersion, Tag: $tagVersion"
+    docker build --build-arg="DRUPAL_VERSION=$buildVersion" -t sajt/drupal:$tagVersion --no-cache .
+    docker tag drupal:$tagVersion sajt/drupal:$tagVersion
+    docker push sajt/drupal:$tagVersion
+}
+
+
 # docker pull php:cli 
 # docker run -ti -v $PWD/xml.php:/xml.php php:cli php /xml.php > /tmp/versions
-IFS=" "
-while read version fullVersion url md5
-do
-    echo "Build: $fullVersion, Tag: $version"
-    BUILDKIT_PROGRESS=plain docker build --build-arg="DRUPAL_VERSION=$fullVersion" -t sajt/drupal:$version .
-    docker push sajt/drupal:$version
-    echo "Build: $fullVersion, Tag: $fullVersion"
-    BUILDKIT_PROGRESS=plain docker build --build-arg="DRUPAL_VERSION=$fullVersion" -t sajt/drupal:$fullVersion .
-    docker push sajt/drupal:$fullVersion
+# IFS=" "
+version=9.1
+fullVersion=9.1.0
+# url=https://ftp.drupal.org/files/projects/drupal-9.1.0.tar.gz #not used
+# while read version fullVersion url md5
+#do
+
+    buildVersion=$fullVersion
+    tagVersion=$version
+    myBuild
+
+    buildVersion=$fullVersion
+    tagVersion=$fullVersion
+    myBuild
+
     if [ "$version" == "9.1" ]; then
-        echo "Build: $fullVersion, Tag: latest"
-        BUILDKIT_PROGRESS=plain docker build --build-arg="DRUPAL_VERSION=$fullVersion" -t sajt/drupal:latest .
-        docker push sajt/drupal:latest
+        buildVersion=$fullVersion
+        tagVersion="latest"
+        myBuild
     fi
-done < versions
+#done < versions
+
---
+++ tempBuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Ez csak a teszteléshez van itt
+
+
+myBuild() {
+    echo $buildversion
+}
+buildversion=1
+myBuild
